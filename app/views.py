c4cd106bd6108b94623b16c4e8c6891b7735cca7
abhinavmabhi <dexmarketing192018@gmail.com>
2025-04-07 10:58:11 +0530

    added the new images

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from .models import Services
 from app.forms import Contact_Form
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.utils.timezone import localtime
 # Create your views here.
 
 
@@ -21,21 +22,35 @@ def Condact_view(request):
         if form.is_valid():
             messages.success(request,"Successfully submitted the Form")
             contact=form.save()
+            formatted_date = localtime(contact.choose_date).strftime("%d-%m-%y %H:%M")
+
             subject=f"New Enquiry Submitted - {contact.service}"
+
             message=f"""
-            Service: {contact.service}
-            Name:{contact.name}
-            Phone:{contact.phone}
-            email:{contact.email}
-            Date choose{contact.choose_date}
-            Message: {contact.message}            
+
+Service: {contact.service}
+Name: {contact.name}
+Phone: {contact.phone}
+email: {contact.email}
+Date: {formatted_date}
+Message: {contact.message}            
 """
+            
+            html_message = f"""
+            <p><strong>Service:</strong> {contact.service}</p>
+            <p><strong>Name:</strong> {contact.name}</p>
+            <p><strong>Phone:</strong> <a href="tel:{contact.phone}">{contact.phone}</a></p>
+            <p><strong>Email:</strong> {contact.email}</p>
+            <p><strong>Date:</strong> {formatted_date}</p>
+            <p><strong>Message:</strong> {contact.message}</p>
+            """
             send_mail(
                 subject=subject,
                 message=message,
                 from_email="abhinavabhi192018@gmail.com",
-                recipient_list=["abhinavabhi192018@gmail.com"],
-                fail_silently=False
+                recipient_list=["abhinavvvv.m@gmail.com"],
+                fail_silently=False,
+                html_message=html_message,
             )
         else:
             messages.error(request,"Form submission failed. Please try again")
@@ -53,3 +68,7 @@ def Service_view(request):
 
 def gallery(request):
     return render(request,"gallery.html")
+
+
+# def Food(request):
+#     return render
